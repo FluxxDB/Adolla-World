cff2808a5c113b470f15d2ffdcbafd2da49f9039
Andrew Calderipe da Rosa <andrewcalderipedarosa@gmail.com>
2021-07-15 17:25:36 -0300

    Structure start

---
+++ src/world/src/main.ts
@@ -0,0 +1,13 @@
+import lootTable from "@rbxts/lootTable";
+
+lootTable(
+	new Map<string, number>([
+		["Potato", 1 / 40],
+		["Carrot", 1 / 30],
+		["Apple", 1 / 20],
+		["Orange", 1 / 9],
+		["Beans", 1 / 1],
+	]),
+);
+
+export {};
